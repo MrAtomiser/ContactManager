553b5a1c24e55cd5c18d1faab8dd2483ee8b718c
mrtomisin <oluwatomisin@meltwater.org>
2017-09-20 21:01:47 +0000

    almost solved this thing, believe in me, guys

--- static_field_homework.py
+++ static_field_homework.py
@@ -3,5 +3,13 @@ class Mest:
 
 	def __init__(self, name, country):
         self.name = name
-        self.country = coountry
+        self.country = country
         Mest.fellows_added += 1
+
+ 	def add_fellows(self):
+ 		if Mest.fellows_added =< 4:
+ 			print ("There are in the list")
+ 		else:
+ 			print ("Exception: We cannot add {} from the country of {} , we are at max capacity".format(self.name, self.country))
+
+ 
